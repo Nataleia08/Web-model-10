765cb5a2d28c3fd381fefb31b64128a406e30b50
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-04-16 22:00:56 +0300

    start draft

---
+++ my_site_hw10/polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+
+class Authors(models.Model):
+
+    fullname = models.CharField(max_length=200)
+    born_date = models.DateField()
+    born_location = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+
+
+class Quotes(models.Model):
+    tags = ArrayField(models.CharField(max_length=30), size=10)
+    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    quote = models.CharField(max_length=2000)
