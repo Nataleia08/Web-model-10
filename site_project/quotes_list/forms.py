7de2b5fb7846c29f385aef2267f2766292a65dc7
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-04-23 23:14:40 +0300

    draft 8

--- site_project/quotes_list/forms.py
+++ site_project/quotes_list/forms.py
@@ -4,21 +4,21 @@ from django.forms import ModelForm
 from .models import Authors, Quotes
 
 class CreateQuoteForm(ModelForm):
-    author = forms.ChoiceField(choices=models.Authors.fullname)
+    author = forms.ChoiceField(choices=models.Authors.objects.all())
     quote = forms.CharField(max_length=100, required=True, widget=forms.TextInput())
 
     class Meta:
-        model = models.Quotes
+        model = Quotes
         fields = ["author", "quote"]
         exclude = ['tags']
 
 
 class CreateAuthorForm(ModelForm):
     fullname = forms.CharField(max_length=100, required=True, widget=forms.TextInput())
-    born_date = forms.DateField(max_length=100, required=True, widget=forms.DateInput())
+    born_date = forms.DateField(required=True, widget=forms.DateInput())
     born_location = forms.CharField(max_length=100, required=True, widget=forms.TextInput())
     description = forms.CharField(max_length=1000, required=True, widget=forms.TextInput())
 
     class Meta:
-        model = models.Quotes
-        fields = ["fullname", "born date", "born location", "description"]
+        model = Authors
+        fields = ["fullname", "born_date", "born_location", "description"]
--- site_project/users/models.py
+++ site_project/users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class UsersSite(models.Model):
@@ -6,7 +8,18 @@ class UsersSite(models.Model):
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     login = models.CharField(max_length=200)
+    avatar = models.ImageField(default='default_avatar.png', upload_to='profile_images')
 
 
     def __str__(self):
         return f"{self.nickname}"
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 250 or img.width > 250:
+            new_img = (250, 250)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
